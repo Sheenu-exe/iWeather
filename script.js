c3dca4a9aa36d40c2ca0a44af8ef6c0fbf265646
Sachin Parihar <130788900+Sheenu-exe@users.noreply.github.com>
2024-02-01 16:40:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ async function checkWeather(city){
         var weather = data.weather[0].main;
         var container =  document.querySelector(".container")
         if (desc =='few clouds'){
+            container.style.backgroundSize="cover"
             container.style.background="url(https://i.pinimg.com/564x/2a/68/29/2a68294a5d806c6c0f6b19d4d8804d56.jpg)";
             Name.style.background="transparent"
         Temp.style.background="transparent"
